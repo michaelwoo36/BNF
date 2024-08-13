2e8e23923998048585c2975433e148d59f183116
Michael Woo <michaelwoo36@gmail.com>
2024-08-13 22:04:44 +0300

    Final commit/push.

--- BNF/src/GrammarMain.java
+++ BNF/src/GrammarMain.java
@@ -1,9 +1,32 @@
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 public class GrammarMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to random sentence generator");
+		System.out.println("what is the name of the grammar file");
+		File grammarTxt = new File(input.next());
+		Scanner inputTxt = new Scanner(grammarTxt);
+		List <String> list = new ArrayList<String>();
+		while(inputTxt.hasNextLine()) {
+			list.add(inputTxt.nextLine());
+		}
+		GrammarSolver object = new GrammarSolver(list);
+		System.out.println("available symbols to generate are:");
+		System.out.println(object.getSymbols());
+		System.out.println("Which symbol do you want to generate, press return to quit");
+		//input.nextLine();
+		String symbolAnswer = input.next();
+		System.out.println("How many times?");
+		int numAnswer = input.nextInt();
+		//input.nextLine();
+		String []master = object.generate(symbolAnswer, numAnswer);
+		for(String i : master) {
+			System.out.println(i);
+		}
 	}
 
 }
+//pn, adj, iv
--- BNF/src/GrammarSolver.java
+++ BNF/src/GrammarSolver.java
@@ -1,4 +1,78 @@
-
+import java.util.*;
 public class GrammarSolver {
-
+	Map <String, String[]> grammars = new TreeMap<String, String[]>();
+	
+	GrammarSolver(List<String>grammar){
+		if(grammar.size() == 0) {
+			throw new IllegalArgumentException("grammar list is empty");
+		}
+		for(String i: grammar) {
+			i.trim();
+			String [] splitProduct = i.split("::=");
+			String [] barSplit = splitProduct[1].split("[|]");
+			grammars.put(splitProduct[0], barSplit);
+		}
+	}
+	
+	boolean grammarContains(String symbol) {
+		
+		return grammars.containsKey(symbol);
+	}
+	
+	
+	String[] generate(String symbol, int times) {
+		
+		if(times < 0 || !grammarContains(symbol)) {
+			throw new IllegalArgumentException();
+		}
+		else {
+			
+			String [] master = new String[times];
+			
+			for(int i = 0; i < times; i++) {
+				master[i] = test(symbol);
+			}
+			return master;
+		}
+		
+	}
+	
+	private String test(String symbol) {
+		
+		Random num = new Random();
+		String [] nonTermValues = grammars.get(symbol);
+		if(nonTermValues == null) {
+			return symbol + " ";
+		}
+		String [] splitTerminals;
+		
+		if(nonTermValues.length > 1) {
+			splitTerminals = nonTermValues[num.nextInt((nonTermValues.length))].split("[ \t]+");
+		}
+		else {
+			splitTerminals = nonTermValues[0].split("[ \t]+");
+			
+		
+		}
+		String gather = "";
+		for(String i : splitTerminals) {
+				 if(i.equals("")) {
+					 continue;
+				 }
+				 else {
+					 gather += test(i);
+				 }
+		}
+		return gather;
+		
+		
+	}
+	String getSymbols() {
+		String nonTerm = "";
+		for(String i: grammars.keySet()) {
+			
+			nonTerm += i + ", ";
+		}
+		return "[" + nonTerm + "]";
+	}
 }
